321864e18337c19e5d11c3075e3a5f27705ed1cc
Thomas Hop <thomas.hhopp@gmail.com>
2020-05-08 21:45:54 +0200

    improved code

--- src/extension.ts
+++ src/extension.ts
@@ -1,70 +1,74 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
-import * as fs from 'fs';
-import * as path from 'path';
+import * as fs from "fs";
+import * as path from "path";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  let disposable = vscode.commands.registerCommand(
+    "blazorcomponents.createComponent",
+    async (event) => {
+      var dirpath: string = event.fsPath;
+      if (fs.lstatSync(dirpath).isFile()) {
+        dirpath = path.dirname(dirpath);
+      }
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "blazorcomponents" is now active!');
+      var relativePath = vscode.workspace.asRelativePath(dirpath, true);
+      var namespace: string = relativePath.split("/").join(".");
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
+      if (relativePath === vscode.workspace.rootPath) {
+        namespace = namespace.split(".").splice(-1)[0];
+      }
 
+      var componentName = await vscode.window.showInputBox({
+        placeHolder: "Component",
+        ignoreFocusOut: true,
+        prompt: "Please enter a component name",
+      });
 
-	let disposable = vscode.commands.registerCommand('blazorcomponents.createComponent', async (event) => {
+      if (componentName === undefined) {
+        return;
+      }
 
+      componentName =
+        componentName.charAt(0).toUpperCase() + componentName.substring(1);
 
-		var dirpath: string = event.fsPath;
-		if (fs.lstatSync(dirpath).isFile()) {
-			dirpath = path.dirname(dirpath);
-		}
+      try {
+        var componentBaseText = await GetTemplateFileAsync("ComponentBase", {
+          ClassName: componentName,
+          Namespace: namespace,
+        });
+        fs.writeFileSync(
+          `${dirpath}/${componentName}.razor.cs`,
+          componentBaseText
+        );
 
-		var relativePath = vscode.workspace.asRelativePath(dirpath, true);
-		var namespace: string = relativePath.split("/").join(".");
+        var componentText = await GetTemplateFileAsync("Component", {
+          ComponentName: componentName,
+        });
+        fs.writeFileSync(`${dirpath}/${componentName}.razor`, componentText);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  );
 
-		if (relativePath === vscode.workspace.rootPath) {
-			namespace = namespace.split(".").splice(-1)[0];
-		}
+  async function GetTemplateFileAsync(templateName: string, args: any) {
+    var template = await vscode.workspace.openTextDocument(
+      context.extensionPath + "/tempaltes/" + templateName
+    );
+    var templateText = template.getText();
+    return StringReplacer(templateText, args);
+  }
 
+  function StringReplacer(str: string, args: any): string {
+    for (var key in args) {
+      str.replace(`%${key}%`, args[key]);
+    }
 
+    return str;
+  }
 
-		var componentName = await vscode.window.showInputBox({ placeHolder: "Component", ignoreFocusOut: true, prompt: "Please enter a component name" });
-
-		if (componentName === undefined) {
-			return;
-		}
-
-		componentName = componentName.charAt(0).toUpperCase() + componentName.substring(1);
-
-		try {
-			vscode.workspace.openTextDocument(context.extensionPath + '/templates/' + "ComponentBase").then((doc: vscode.TextDocument) => {
-				var text = doc.getText();
-				text = text.replace('%ClassName%', componentName);
-				text = text.replace('%Namespace%', namespace);
-				fs.writeFileSync(dirpath + "\\" + componentName + ".razor.cs", text);
-			});
-
-			vscode.workspace.openTextDocument(context.extensionPath + '/templates/' + "Component").then((doc: vscode.TextDocument) => {
-				var text = doc.getText();
-				text = text.replace('%ComponentName%', componentName + "");
-				fs.writeFileSync(dirpath + "\\" + componentName + ".razor", text);
-			});
-
-
-		} catch (e) {
-			console.log(e);
-		}
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
